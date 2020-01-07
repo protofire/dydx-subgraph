d921eafdb25d0e0efd7512911995b9ee3e6ad1c3
Sebastián Galiano <sgaliano@gmail.com>
2020-01-07 14:51:32 -0300

    Handle SAI/DAI price history from oracles

---
+++ src/external/oracles/sai-price.ts
@@ -0,0 +1,19 @@
+import { DaiPriceOracle, PriceSet } from '../../../generated/SaiPriceOracle/DaiPriceOracle'
+
+import { PriceLog, Token } from '../../../generated/schema'
+import { toDecimal } from '../../utils/numberic'
+
+export function handlePriceSet(event: PriceSet): void {
+  let oracle = DaiPriceOracle.bind(event.address)
+
+  let token = Token.load(oracle.DAI().toHexString())
+
+  if (token != null) {
+    let priceLog = new PriceLog(token.id + '-' + event.params.newPriceInfo.lastUpdate.toString())
+    priceLog.asset = token.id
+    priceLog.value = toDecimal(event.params.newPriceInfo.price, token.decimals)
+    priceLog.timestamp = event.block.timestamp
+
+    priceLog.save()
+  }
+}
